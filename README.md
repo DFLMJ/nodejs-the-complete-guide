5982774f7901263a0ccdef83877a76b2ba4042dc
gfxKnight <ma6@outlook.com>
2024-06-02 22:56:29 +0800

    文件输出学习

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nodejs-the-complete-guide
 udemy 的node课程
+课程所使用node版本：14.11
---
+++ node-fist-app/第一节/fist-app.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: MaJun
+ * Dont't Settle
+ * @Date: 2024-06-02 19:33:02
+ * @LastEditTime: 2024-06-02 22:54:47
+ * @LastEditors: MaJun
+ * @Description: I wrote this.
+ * @FilePath: \nodejs-the-complete-guide\node-fist-app\第一节\fist-app.js
+ */
+const fs = require('fs')
+fs.writeFileSync('hello.txt','您好，马俊。未来的领航者')
+console.log('你好 马俊')
